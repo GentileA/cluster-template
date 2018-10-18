934f26b21b75673d1b34b42d016047a13e4d1a71
corbtori15 <43189053+corbtori15@users.noreply.github.com>
2018-10-18 18:53:55 -0400

    Update install_nfs_storage_node.sh

--- install_nfs_storage_node.sh
+++ install_nfs_storage_node.sh
@@ -1,9 +1,11 @@
 #install nfs
 sudo yum install nfs-utils
+
+
 mkdir/scratch
 
 chmod -R 755/var/scratch                
-chown nfsnobody:nfsnobody/var/scratch
+
 
 /scratch  192.168.0.101 (rw,sync,no_root_squash,no_all_squash)
 

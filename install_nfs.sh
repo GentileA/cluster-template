8003dd7b32e5252e10ef2ac91c044aa324485ce1
th786903 <43189005+th786903@users.noreply.github.com>
2018-10-18 17:29:07 -0400

    Update install_nfs.sh

--- install_nfs.sh
+++ install_nfs.sh
@@ -1,3 +1,11 @@
 
-sudo yum -y install nsf-utils   #https://www.howtoforge.com/nfs-server-and-client-on-centos-7 section nsf client end
-mkdir /software 
+sudo yum install nsf-utils        #https://www.howtoforge.com/nfs-server-and-client-on-centos-7
+mkdir /software                   # make directory called /software
+chmod -R 755 /software            # change user permissions 
+
+/software    192.168.0.101(rw,sync,no_root_squash,no_all_squash) # assign ip possibly loop to get more
+
+systemctl restart nfs-server      #start the NFS server
+
+mkdir /scratch
+mkdir -p /mnt/nfs/scratch

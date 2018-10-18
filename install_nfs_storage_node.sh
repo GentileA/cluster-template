712c6acccb109a111af6c4f14b36d7fc86168a1f
corbtori15 <43189053+corbtori15@users.noreply.github.com>
2018-10-18 18:35:45 -0400

    Update install_nfs_storage_node.sh

--- install_nfs_storage_node.sh
+++ install_nfs_storage_node.sh
@@ -6,9 +6,18 @@ chown nfsnobody:nfsnobody/var/scratch
 
 /scratch  192.168.0.101 (rw,sync,no_root_squash,no_all_squash)
 
+systemct1 enable rpcbind
+systemct1 enable nfs-server
+systemct1 enable nfs-lock
+systemct1 enable nfs-idmap
+systemct1 start rpcbind
+systemct1 start nfs-server
+systemct1 start nfs-lock
+systemct1 start nfs-idmap 
+
 systemctl restart nfs-server
 
 mkdir / scratch
 mkdir –p/mnt/nfs/scratch
-touch /mnt/nfs/var/scratch/test_nfs
+
 

7a90a8afd3885142aa20686e86cbb9860b9f7466
th786903 <43189005+th786903@users.noreply.github.com>
2018-10-18 18:55:14 -0400

    Update install_nfs_head_node.sh

--- install_nfs_head_node.sh
+++ install_nfs_head_node.sh
@@ -6,6 +6,15 @@ chmod -R 755 /software                    # change user permissions
 
 /software    192.168.0.1(rw,sync,no_root_squash,no_all_squash) # create share points
 
+systemctl enable rpcbind
+systemctl enable nfs-server
+systemctl enable nfs-lock
+systemctl enable nfs-idmap                #preventing premissions being changed if our host directory is changed
+systemctl start rpcbind
+systemctl start nfs-server
+systemctl start nfs-lock
+systemctl start nfs-idmap
+
 systemctl restart nfs-server              #start the NFS server
 
 mkdir /scratch                            #make directory to be mounted      

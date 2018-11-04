8062aea2ddd5c01bdc874b87ac4d2321f98bdb2d
th786903 <43189005+th786903@users.noreply.github.com>
2018-11-04 18:54:50 -0500

    Update passwordless.sh

--- passwordless.sh
+++ passwordless.sh
@@ -91,6 +91,9 @@ cp $PRIVKEY ${CUSTOM_SSHDIR}
 cp $PUBKEY ${CUSTOM_SSHDIR}
 cat $PUBKEY >> ${CUSTOM_SSHDIR}/authorized_keys
 chown -R ${CUSTOM_USER}:${CUSTOM_GROUP} ${CUSTOM_SSHDIR}
-su ag781693 -c 'echo "StrictHostKeyChecking no" > ${CUSTOM_SSHDIR}/config'
+sudo touch /users/ag781693/.ssh/.config
+sudo echo "StrictHostKeyChecking no" | sudo tee -append /users/ag781693/.ssh/.config
+
+#su ag781693 -c 'echo "StrictHostKeyChecking no" > ${CUSTOM_SSHDIR}/config'
 
 exit 0
